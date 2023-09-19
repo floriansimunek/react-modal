6350406ab2d89ade8fe32c7fa88c6b61aa7399ed
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-19 18:53:58 +0200

    ✨ Call CloseIcon component

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,4 +1,5 @@
 import Button from '@components/Button';
+import CloseIcon from '@components/CloseIcon';
 import { DialogProps } from '@types';
 import './index.scss';
 
@@ -12,6 +13,7 @@ const Dialog: React.FC<DialogProps> = ({ children, onClick }) => {
       <div className="modal__dialog" data-testid="modal__dialog">
         <div className="modal__dialog--header">
           <p className="modal__dialog--header-title">Title</p>
+          <CloseIcon onClick={onClick} />
         </div>
         <div className="modal__dialog--body">{children}</div>
         <div className="modal__dialog--footer">
