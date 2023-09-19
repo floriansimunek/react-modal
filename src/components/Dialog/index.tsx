b30c5e196bf432a86adaf77bc641b3a48ed6a50f
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-19 11:29:46 +0200

    💄 Add CSS

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,7 +1,12 @@
 import { DialogProps } from '@types';
+import './index.scss';
 
 const Dialog: React.FC<DialogProps> = ({ children }) => {
-  return <div className="modal--dialog">{children}</div>;
+  return (
+    <div className="modal--container">
+      <div className="modal--dialog">{children}</div>
+    </div>
+  );
 };
 
 export default Dialog;
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,6 +2,7 @@ import Button from '@components/Button';
 import Dialog from '@components/Dialog';
 import { ModalProps } from '@types';
 import { useState } from 'react';
+import './index.scss';
 
 const Modal: React.FC<ModalProps> = ({ children }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
