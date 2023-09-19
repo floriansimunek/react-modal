56f726f49d2d4b0c5c612c6eda6f6803e1e833fa
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-19 23:44:28 +0200

    ✅ Add tests

--- src/__tests__/Modal.test.tsx
+++ src/__tests__/Modal.test.tsx
@@ -1,6 +1,7 @@
 import Modal from '@components/Modal';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
 describe('Modal', () => {
   describe('When component called', () => {
@@ -26,6 +27,22 @@ describe('Modal', () => {
     it('should render the Modal', () => {
       const modal = screen.getByTestId('modal');
       expect(modal).toBeInTheDocument();
+    });
+  });
+
+  describe('When component called with onOpen event', () => {
+    it('should call onOpen function when menu is opened', () => {
+      const fn = jest.fn();
+      render(<Modal onOpen={fn}></Modal>);
+      const button = screen.getByTestId('modal__button');
+      const container = screen.getByTestId('modal__container');
+
+      expect(fn).toBeCalledTimes(0);
+      act(() => button.click());
+      expect(fn).toBeCalledTimes(1);
+      act(() => container.click());
+      act(() => button.click());
+      expect(fn).toBeCalledTimes(2);
     });
   });
 });
