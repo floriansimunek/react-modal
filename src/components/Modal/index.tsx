8251b375f438dcf8c75be3fade50d302e6518568
Florian SIMUNEK <florian.simunek@gmail.com>
2023-09-19 23:36:39 +0200

    ✨ Add onOpen prop

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,11 +1,15 @@
 import Button from '@components/Button';
 import Dialog from '@components/Dialog';
 import { ModalProps } from '@types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './index.scss';
 
-const Modal: React.FC<ModalProps> = ({ children, title }) => {
+const Modal: React.FC<ModalProps> = ({ children, title, onOpen }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    isOpen ? onOpen && onOpen() : null;
+  }, [isOpen, onOpen]);
 
   return (
     <div className="modal" data-testid="modal" data-rm-is-open={isOpen}>
