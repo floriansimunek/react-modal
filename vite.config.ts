220e7c76800669871c0038b58daaba285c4d8c8a
Florian SIMUNEK <60973568+floriansimunek@users.noreply.github.com>
2023-09-21 13:09:34 +0200

    ✨ Feature#30 publish to npm (#31)
    * 🔧 Add config
    
    * ✨ Add export file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,47 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import * as packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    cssInjectedByJsPlugin(),
+    dts({
+      include: ['src/components/'],
+    }),
+  ],
   resolve: {
     alias: {
       '@components': '/src/components',
       '@types': '/src/types/index.d.ts',
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve('src', 'components/index.ts'),
+      name: 'React-Modal',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-modal.${format}.js`,
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(packageJson.peerDependencies),
+        'react',
+        'react-dom',
+      ],
+      output: {
+        globals: {
+          react: 'react',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
     },
   },
 });
